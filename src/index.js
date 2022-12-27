b46cb1e05b84a2be6f3b021fed06ef61458434be
André Luiz <andregmdias@gmail.com>
2022-12-27 20:29:19 -0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const cors = require('cors');
+
+const {v4: uuidv4} = require('uuid');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const users = [];
+
+function checksExistsUserAccount(request, response, next) {
+  const { username } = request.headers;
+  
+  const user = users.find((user) => user.username === username);
+  
+  if (!user) {
+    return response.status(404).json({error: "User does not exist !"})
+  }
+  
+  return next();
+}
+
+app.post('/users', (request, response) => {
+  const {name, username} = request.body;
+
+  users.push({
+    id: uuidv4(),
+    name,
+    username,
+    todos: [],
+  });
+});
+
+app.get('/todos', checksExistsUserAccount, (request, response) => {
+  
+});
+
+app.post('/todos', checksExistsUserAccount, (request, response) => {
+  // Complete aqui
+});
+
+app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
+  // Complete aqui
+});
+
+app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
+  // Complete aqui
+});
+
+app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
+  // Complete aqui
+});
+
+module.exports = app;
